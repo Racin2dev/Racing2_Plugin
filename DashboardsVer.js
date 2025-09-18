8dbd7dfd55e7a417c98515df999327132c590ed5
Racing2 <racing2dev@outlook.com>
2025-09-18 20:36:06 +0800

    Add files via upload

---
+++ DashboardsVer.js
@@ -0,0 +1,14 @@
+{
+  "dashboards": [
+    {
+      "Name": "Racing2-Porsche 963 GTP Dashboard",
+      "Version": "1.0",
+      
+    },
+    {
+      "Name": "Racing2-BMW-M4-GT3",
+      "Version": "1.0",
+      
+    }
+  ]
+}
